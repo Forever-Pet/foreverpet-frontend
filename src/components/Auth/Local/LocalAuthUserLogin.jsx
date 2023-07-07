a6be1f1f286c7f28347af131e0ec5d4559fb0f6a
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-07-08 03:58:11 +0900

    :beers: Not Yet Login code drunkenly.

--- src/components/Auth/Local/LocalAuthUserLogin.jsx
+++ src/components/Auth/Local/LocalAuthUserLogin.jsx
@@ -1,6 +1,8 @@
+// CSS
+import classes from "../../../styles/css/components/Auth/Local/LocalAuthUser.module.css";
+
 const LocalAuthUserLogin = () => {
   return <div>쇼핑몰 자체 로그인</div>;
 };
 
-
-export default LocalAuthUserLogin
+export default LocalAuthUserLogin;
