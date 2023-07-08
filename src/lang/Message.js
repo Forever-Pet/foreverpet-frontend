4e38bac74c8c511423ae942e29ab54ef455cac28
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-07-09 02:52:55 +0900

    :memo: Add lang message File

---
+++ src/lang/Message.js
@@ -0,0 +1,12 @@
+export const MSG = {
+  JOIN: {
+    NAME_FAIL: "이름을 입력 해주세요",
+    EMAIL_FAIL: "이메일 형식이 올바르지 않습니다.",
+    EMAIL_ONLY_USED: "해당 이메일은 사용중입니다.",
+    EMAIL_AUTH_WRONG: "이메일 인증번호가 올바르지 않습니다.",
+    EMAIL_AUTH_VALUE: "인증번호를 입력 해주세요",
+    EMAIL_AUTH_FAIL: "이메일 인증을 완료 해주세요",
+    PASSWORD_FAIL: "비밀번호가 서로 일치하지 않습니다.",
+    TEL_FAIL: "휴대폰 번호 형식이 올바르지 않습니다.",
+  },
+};
