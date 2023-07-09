f065aec4d10d0cd2c7aae6cad699fff7d73689cc
Bella <88245113+shineju@users.noreply.github.com>
2023-07-09 21:42:49 +0900

    필터 ui 추가
    필터 ui 추가, 데이터 코드는 나중에,,

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,17 +1,56 @@
 import styles from '../../styles/css/components/Product/Product.module.css'
 import ProductItem from '../../components/Card/ProductItem'
+import { MdArrowDropDown, MdArrowDropUp } from "react-icons/md";
+import { ClickHook } from '../../hooks/ClickHook/ClickHook'
+import { useState } from 'react';
+import { BiCheck } from "react-icons/bi";
+
+const ListItem = ({value, onClick, isChecked}) => {
+  return (
+    <li className={styles.selectDrop__list__option} style={{color: isChecked ? 'black':'gray'}} onClick={onClick}>
+      {isChecked && 
+      <BiCheck className={styles.selectDrop__list__option__check}/>
+      }
+      {value}
+    </li>
+  )
+}
 
 const Product = (props) => {
+
+  const [click, checkClick] = ClickHook(false)
+  const [selectedValue, setSelectedValue] = useState('베스트');
+  
+  const handleItemClick = (value) => {
+    setSelectedValue(value);
+  };
+
   return <>
   <div className={styles.base}>
     <div>
-        <h1>사료</h1>
-        <span style={ {float: 'left'} }>11개의 상품</span>
-        <span style={ {float:'right'} }>베스트상품순</span>
-        <div style={ {clear:'both'} }></div>
-        <hr></hr>
+        <h1>제품명</h1>
+        <div className={styles.filterBox}>
+          <span>상품 개수</span>
+          <div className={styles.filterBox__filter}  onClick={checkClick}>
+            <span>{selectedValue}</span>
+            {click ? <MdArrowDropUp />: <MdArrowDropDown />}
+          </div>
+        </div>
+      <hr></hr>
     </div>
-    <ProductItem></ProductItem>
+
+    {click &&  <div className={styles.selectDrop__cont}>
+      <ul className={styles.selectDrop__list}>
+        <ListItem value="베스트" isChecked={selectedValue === "베스트"} onClick={() => handleItemClick("베스트")}/>
+
+        <ListItem value="최신순" isChecked={selectedValue === "최신순"} onClick={() => handleItemClick("최신순")}/>
+      </ul>
+    </div> }
+
+    <div className={styles.container}>
+      <ProductItem></ProductItem>
+    </div>
+
   </div>
   
   </>
