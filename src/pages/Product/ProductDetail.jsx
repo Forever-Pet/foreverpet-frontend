5a2dbb0a0a93448d2f4a23b58fd6c0dffc89690f
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-07-18 08:12:59 +0000

    :lipstick: Design : 상품상세 정보 라우터 전체 반응형 디자인 수정

--- src/pages/Product/ProductDetail.jsx
+++ src/pages/Product/ProductDetail.jsx
@@ -9,15 +9,18 @@ const ProductDetail = () => {
   return (
     <div className={styles["product-detail__view-wrap"]}>
       <div className={styles["header"]}>헤더임</div>
-      <div className={styles["test"]}>
-        <div className={styles["product-detail__view-container"]}>
-          <div className={styles["product-detail__view--information"]}>
-            <ProductDetailInformation />
-          </div>
-          <div className={styles["product-detail__view--PriceCard"]}>
-            <ProductDetailPriceCard />
-          </div>
+
+
+      <div className={styles["product-detail__view-container"]}>
+
+        <div className={styles["product-detail__view--information"]}>
+          <ProductDetailInformation />
         </div>
+
+        <div className={styles["product-detail__view--PriceCard"]}>
+          <ProductDetailPriceCard />
+        </div>
+        
       </div>
     </div>
   );
