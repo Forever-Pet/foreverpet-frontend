01c78756c527396fa6eaadd5f322788b9bccbc70
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-07-19 20:43:49 +0900

    :recycle: Refactor : 카카오 토큰 api 요청 코드 리팩토링

--- src/components/Auth/Kakao/KakaoCallback.jsx
+++ src/components/Auth/Kakao/KakaoCallback.jsx
@@ -3,33 +3,36 @@ import axios from "axios";
 
 const KakaoCallback = () => {
   useEffect(() => {
-    const params = new URL(document.location.toString()).searchParams;
-    const code = params.get("code");
-    const grantType = "authorization_code";
-    const CLIENT_ID = "a6bfe93b460dd365f9951854df478ab9";
-    const REDIRECT_URL = "http://localhost:5173/oauth/kakaoCallback";
-
-    const data = async () => {
+    const kakaoDataAxios = async () => {
       try {
+        const params = new URL(document.location.toString()).searchParams;
+        const CODE = params.get("code");
+        const GRANT_TYPE = "authorization_code";
+        const CLIENT_ID = "a6bfe93b460dd365f9951854df478ab9";
+        const REDIRECT_URL = "http://localhost:5173/oauth/kakaoCallback";
+        const KAKAO_ACCESS_URL = `https://kauth.kakao.com/oauth/token?grant_type=${GRANT_TYPE}&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&code=${CODE}`;
+        const KAKAO_USER_INFO_URL = `https://kapi.kakao.com/v2/user/me`;
+        const ACCESS_HEADERS_CONTENT_TYPE = {
+          "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+        };
         const res = await axios.post(
-          `https://kauth.kakao.com/oauth/token?grant_type=${grantType}&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&code=${code}`,
+          KAKAO_ACCESS_URL,
           {},
           {
-            headers: {
-              "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-            },
+            headers: ACCESS_HEADERS_CONTENT_TYPE,
           }
         );
-
         const { access_token } = res.data;
+        const TOEKN_HEADERS_CONTENT_TYPE = {
+          Authorization: `Bearer ${access_token}`,
+          "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+        };
+
         const res_axios = await axios.post(
-          `https://kapi.kakao.com/v2/user/me`,
+          KAKAO_USER_INFO_URL,
           {},
           {
-            headers: {
-              Authorization: `Bearer ${access_token}`,
-              "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-            },
+            headers: TOEKN_HEADERS_CONTENT_TYPE,
           }
         );
         console.log(res_axios);
@@ -37,10 +40,10 @@ const KakaoCallback = () => {
         console.log(error);
       }
     };
-    data();
+    kakaoDataAxios();
   }, []);
 
-  return <div>카카오 콜백</div>;
+  return <></>;
 };
 
 export default KakaoCallback;
