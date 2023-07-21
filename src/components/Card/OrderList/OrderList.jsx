cee7d9021e84fa4d4ff3177752ab4ae80a8a981a
[LK1] <[ssomcandy777@gmail.com]>
2023-07-21 17:49:46 +0900

    :lipstick: Design: 상품 결제 품목 리스트 디자인 작업

---
+++ src/components/Card/OrderList/OrderList.jsx
@@ -0,0 +1,54 @@
+// CSS
+import styles from '../../../styles/css/components/Card/OrderList/OrderList.module.css';
+
+// Components
+import Image from '../../../common/Img/Image';
+
+// Dummy data
+const data = [
+  {
+    id: 1,
+    imageUrl:
+      'https://www.bohori.com/web/product/medium/202304/8c40e60df9ad25fdd15c40f72b779ede.jpg',
+    name: '채식 애견껌 스틱형 120g (12개임)-오리지널 맛 (초록색) 맛있어요 !대박임 !',
+    quantity: 2,
+    totalAmount: 9000,
+  },
+  {
+    id: 2,
+    imageUrl:
+      'https://www.bohori.com/web/product/medium/202304/a5eaff3c46627bd4f906aecf541bf549.jpg',
+    name: '채식 애견껌 스틱형 120g (12개입)-과일 맛 (베이지색)',
+    quantity: 1,
+    totalAmount: 4500,
+  },
+];
+
+const OrderList = (props) => {
+  return (
+    <ul>
+      {data?.map((productItem, _) => {
+        return (
+          <li key={productItem.id} className={styles['order-list__item']}>
+            <Image
+              src={productItem.imageUrl}
+              className="order-list__image"
+              alt=""
+            />
+            <span className={styles['order-list__item--middle']}>
+              <span className={styles['order-list__item--name']}>
+                {productItem.name}
+              </span>
+              <span className={styles['order-list__item--quantity']}>
+                {productItem.quantity}개
+              </span>
+            </span>
+            <span>{productItem.totalAmount}원</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default OrderList;
