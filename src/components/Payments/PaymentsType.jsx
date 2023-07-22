b80e2843a98a36e5fcc9f7f96e814cb16134a30a
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-07-23 03:28:09 +0900

    :lipstick: Design: 결제 페이지 결제 수단 디자인 추가

--- src/components/Payments/PaymentsType.jsx
+++ src/components/Payments/PaymentsType.jsx
@@ -1,5 +1,21 @@
+// CSS
+import styles from "../../styles/css/components/Payments/PaymentsType.module.css";
+
+// Components
+import PaymentsInputHeader from "./PaymentsInputHeader/PaymentsInputHeader";
+
 const PaymentsType = () => {
-  return <div>결제유형</div>;
+  return (
+    <>
+      <PaymentsInputHeader title="결제수단" />
+      <div className={styles["payments-type"]}>
+        <div className={styles["payments-type__text"]}>결제수단 선택</div>
+        <div className={styles["payments-type__list"]}>
+          <span className={styles["payments-type__kakaopay"]}>카카오페이</span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default PaymentsType;
