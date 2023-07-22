93c2483e8448b53a4b2ba42721088210f868cdd9
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-07-23 03:31:45 +0900

    :sparkles: Feat: 결제 페이지 기초 작업

---
+++ src/pages/Payments/Payments.jsx
@@ -0,0 +1,50 @@
+// CSS
+import styles from "../../styles/css/pages/Payments/Payments.module.css";
+
+// Components
+import Title from "../../common/Title/Title";
+import PaymentsAmountInfo from "../../components/Payments/PaymentsAmountInfo";
+import PaymentsOrderInfo from "../../components/Payments/PaymentsOrderInfo";
+import PaymentsOwnerInfo from "../../components/Payments/PaymentsOwnerInfo";
+import PaymentsDeliveryInfo from "../../components/Payments/PaymentsDeliveryInfo";
+import PaymentsType from "../../components/Payments/PaymentsType";
+
+const Payments = () => {
+  return (
+    <div className={styles["payments-wrap"]}>
+      <div
+        style={{
+          width: "100%",
+          height: "202px",
+          position: "fixed",
+          backgroundColor: "teal",
+          zIndex: "20",
+        }}
+      >
+        헤더임
+      </div>
+      <div className={styles["payments-info"]}>
+        <div className={styles["payments-text"]}>
+          <Title title="주문결제" />
+        </div>
+        <div>
+          <PaymentsOrderInfo />
+        </div>
+        <div>
+          <PaymentsOwnerInfo />
+        </div>
+        <div>
+          <PaymentsDeliveryInfo />
+        </div>
+        <div>
+          <PaymentsType />
+        </div>
+      </div>
+      <div className={styles["payments-info-amount"]}>
+        <PaymentsAmountInfo />
+      </div>
+    </div>
+  );
+};
+
+export default Payments;
--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -1,19 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 // Pages
-import Home from './Home';
-import Login from './User/Login';
-import Join from './User/Join';
-import KakaoCallback from '../components/Auth/Kakao/KakaoCallback';
-import FeedProduct from './Product/FeedProduct';
-import ProductDetail from './Product/ProductDetail';
-import ProductPayments from './Product/Payments/ProductPayments';
-import PageNotFound from './PageNotFound/PageNotFound';
+import Home from "./Home";
+import Login from "./User/Login";
+import Join from "./User/Join";
+import KakaoCallback from "../components/Auth/Kakao/KakaoCallback";
+import FeedProduct from "./Product/FeedProduct";
+import ProductDetail from "./Product/ProductDetail";
+import Payments from "./Payments/Payments";
+import PageNotFound from "./PageNotFound/PageNotFound";
 
 // Test
-import PaymentsAmountInfo from '../components/Payments/PaymentsAmountInfo'; // 결제 페이지 금액 정보창
-import OrderList from '../components/Card/OrderList/OrderList';
+import PaymentsAmountInfo from "../components/Payments/PaymentsAmountInfo"; // 결제 금액 정보창
+import PaymentsOrderInfo from "../components/Payments/PaymentsOrderInfo"; // 결제 주문상품 컴포넌트
+import PaymentsOwnerInfo from "../components/Payments/PaymentsOwnerInfo"; // 결제 주문자 정보 컴포넌트
+import PaymentsDeliveryInfo from "../components/Payments/PaymentsDeliveryInfo"; // 결제 배송지 정보 컴포넌트
+import PaymentsType from "../components/Payments/PaymentsType"; // 결제수단 컴포넌트
 
 const Router = () => {
   const [isInLogged, setisInLogged] = useState(true);
@@ -28,9 +31,9 @@ const Router = () => {
           <Route path="/oauth/kakaoCallback" element={<KakaoCallback />} />
           <Route path="/product/feed" element={<FeedProduct />} />
           <Route path="/product/detail/:id" element={<ProductDetail />} />
-          <Route path="/product/payments" element={<ProductPayments />} />
+          <Route path="/payments" element={<Payments />} />
           <Route path="/*" element={<PageNotFound />} />
-          <Route path="/test" element={<OrderList />} />
+          <Route path="/hi" element={<PaymentsType />} />
         </Routes>
       ) : (
         <Routes>
