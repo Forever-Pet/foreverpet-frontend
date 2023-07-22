ea06ef1d0d0ac4154b983dd26dab5bf2bf27308b
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-07-23 03:27:33 +0900

    :lipstick: Design: 결제 페이지 상품금액 창 디자인 수정

--- src/components/Payments/PaymentsAmountInfo.jsx
+++ src/components/Payments/PaymentsAmountInfo.jsx
@@ -1,11 +1,11 @@
 // CSS
-import styles from '../../styles/css/components/Product/Payments/PaymentsAmountInfo.module.css';
+import styles from "../../styles/css/components/Payments/PaymentsAmountInfo.module.css";
 
 // Utils
-import { addPriceAmount } from '../../utils/addPriceComma';
+import { addPriceAmount } from "../../utils/addPriceComma";
 
 // Compoennts
-import Title from '../../common/Title/Title';
+import Title from "../../common/Title/Title";
 
 // Dummy Data
 const data = {
@@ -18,23 +18,23 @@ const PaymentsAmountInfo = (props) => {
   const { totalAmount, deliveryPrice, couponSale } = data;
 
   return (
-    <div className={styles['payment-amount__info']}>
-      <div className={styles['payment-amount__info--text']}>
+    <div className={styles["payment-amount__info"]}>
+      <div className={styles["payment-amount__info--text"]}>
         <Title title="상품금액" />
       </div>
-      <div className={styles['payment-amount__card']}>
-        <div className={styles['payment-amount__card--desc']}>
+      <div className={styles["payment-amount__card"]}>
+        <div className={styles["payment-amount__card--desc"]}>
           <div>총 상품금액</div>
           <div>총 배송료</div>
           <div>쿠폰 할인</div>
         </div>
-        <div className={styles['payment-amount__card--amount']}>
+        <div className={styles["payment-amount__card--amount"]}>
           <div>{addPriceAmount(totalAmount)} 원</div>
           <div>{addPriceAmount(deliveryPrice)} 원</div>
           <div>-{addPriceAmount(couponSale)} 원</div>
         </div>
       </div>
-      <div className={styles['payment-amount__card--totalAmount']}>
+      <div className={styles["payment-amount__card--totalAmount"]}>
         <div>최종 결제금액</div>
         <div>{addPriceAmount(totalAmount)}원 </div>
       </div>
