cba847364ea88e90c21f1d8d2c388ada641cb42e
최은하 <choieunha@choeeunhaui-MacBookAir.local>
2023-07-23 12:47:16 +0900

    헤더 탑 메뉴 , 네비바박스 css 수정 & 스넥 카테고리 컴포넌트 생성

--- src/components/Navigation/Category/Snack.jsx
+++ src/components/Navigation/Category/Snack.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const Snack = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-};
-
+const Snack = [
+  {id: '0' , src : 'img/snack1.jpg' , name: '고위드테일 소고기 크런칩 85g', price: '15,000원' },
+  {id: '1' , src : 'img/snack2.jpg' , name: '고위드테일x포에버펫 슈퍼바이츠 70g', price: '8,9000원' },
+  {id: '2' , src : 'img/snack3.jpg' , name: '고위드테일 슈퍼바 25g', price: '28,500원' },
+  {id: '3' , src : 'img/snack6.jpg' , name: '아더마스 프로틴볼 베지미트볼', price: '14,000원' },
+  {id: '4' , src : 'img/snack7.jpg' , name: '스위피 오독오독 바삭', price: '15,900원' },
+  {id: '5' , src : 'img/snack8.jpg' , name: '코코테라피 블루베리 코블러', price: '18,000원' },
+  {id: '6' , src : 'img/snack9.jpg' , name: '빅스비 파크팝스 크런치', price: '16,000원' },
+  {id: '7' , src : 'img/snack10.jpg' , name: '단호박 코코넛 파운드', price: '7,200원' },
+  {id: '8' , src : 'img/snack11.jpg' , name: '포포네 가지가지치킨비스킷', price: '5,400원' },
+  {id: '9' , src : 'img/snack12.jpg' , name: '씽크라이크펫 비건 해피트릿', price: '5,300원' }
+];
 export default Snack;
