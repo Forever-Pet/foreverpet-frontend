b56930e89996fd09715a1327c26cbc15277a9d48
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-07-23 19:40:21 +0900

    :sparkles: Feat: 결제 페이지 상품 금액정보 창 계산 기능 추가

--- src/components/Payments/PaymentsAmountInfo.jsx
+++ src/components/Payments/PaymentsAmountInfo.jsx
@@ -1,21 +1,49 @@
+// React Hooks
+import { useEffect, useState } from "react";
+
 // CSS
 import styles from "../../styles/css/components/Payments/PaymentsAmountInfo.module.css";
 
 // Utils
-import { addPriceAmount } from "../../utils/addPriceComma";
+import { addPriceComma } from "../../utils/addPriceComma";
 
 // Compoennts
 import Title from "../../common/Title/Title";
 
-// Dummy Data
-const data = {
-  totalAmount: 13500,
-  deliveryPrice: 0,
-  couponSale: 0,
-};
-
 const PaymentsAmountInfo = (props) => {
-  const { totalAmount, deliveryPrice, couponSale } = data;
+  const [productAmountInfo, setProductAmountInfo] = useState(
+    props.dummyOrderListData
+  );
+
+  const [productAmount, setProductAmount] = useState({
+    productAmount: 0,
+    productDeliveryAmount: 0,
+    productCouponAmount: 0,
+    productTotalAmount: 0,
+  });
+
+  useEffect(() => {
+    // 각각 가정한 배송료와 쿠폰 할인 값
+    const deliveryAmount = 0;
+    const couponDiscountAmount = 0;
+
+    const productAmount = productAmountInfo.reduce(
+      (total, item) => total + item.quantity * item.defaultAmount,
+      0
+    );
+
+    // 결제 총금액
+    const totalAmount = productAmount + deliveryAmount - couponDiscountAmount;
+
+    setProductAmount({
+      productAmount: productAmount,
+      productDeliveryAmount: deliveryAmount,
+      productCouponAmount: couponDiscountAmount,
+      productTotalAmount: totalAmount,
+    });
+    // 결제금액 상향식 통신으로 전송
+    props.paymentsAmountBtn(totalAmount);
+  }, []);
 
   return (
     <div className={styles["payment-amount__info"]}>
@@ -29,14 +57,14 @@ const PaymentsAmountInfo = (props) => {
           <div>쿠폰 할인</div>
         </div>
         <div className={styles["payment-amount__card--amount"]}>
-          <div>{addPriceAmount(totalAmount)} 원</div>
-          <div>{addPriceAmount(deliveryPrice)} 원</div>
-          <div>-{addPriceAmount(couponSale)} 원</div>
+          <div>{addPriceComma(productAmount.productAmount)}원</div>
+          <div>{addPriceComma(productAmount.productDeliveryAmount)}원</div>
+          <div>{`-${addPriceComma(productAmount.productCouponAmount)}`}원</div>
         </div>
       </div>
       <div className={styles["payment-amount__card--totalAmount"]}>
         <div>최종 결제금액</div>
-        <div>{addPriceAmount(totalAmount)}원 </div>
+        <div>{addPriceComma(productAmount.productTotalAmount)}원</div>
       </div>
     </div>
   );
