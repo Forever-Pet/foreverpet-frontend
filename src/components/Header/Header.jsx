6aec53740bd4b8a39e64b722c6eb38f733df25aa
최은하 <choieunha@choeeunhaui-MacBookAir.local>
2023-07-24 22:32:05 +0900

    헤더 상단 베너 삭제 기능 & css 수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,20 +4,16 @@ import NavBar from "./NavBar";
 
 // CSS
 import styles from "../../styles/css/components/Header/Header.module.css";
+import HeaderBan from "../Banner/HeaderBan";
 
 //간식 누른거의 데이터와 props 로 엘라님 컴포넌트 전달 해 주기
-//css bem 으로 바꾸기
-//스크롤 내리면 헤더 안보이게
-//ES6 바꾸기 ,엑스 클릭 , 모달 컴포넌트 폴더 따로 만들어서 설정
-
 
 //스크롤 헤더 값 변경 구현 예정
 //헤더 스크롤 핸들러
 const Header = (props) => {
-  
   return (
     <header className={styles.header}>
-      {/* <button onClick={HeaderBan}><RxCross2/></button> */}
+      <HeaderBan/>
       <div className={styles.contents}>
         <div className={styles.logo}>포에버펫</div>
       </div>
