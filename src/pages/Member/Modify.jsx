3322b875fb23290c75001865bb844a4b5141772b
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-07-26 08:41:47 +0000

    :sparkles: Feat: 재사용 컴포넌트 개발, 모디파이 라우터 임시 디자인  적용

--- src/pages/Member/Modify.jsx
+++ src/pages/Member/Modify.jsx
@@ -2,11 +2,32 @@
 import styles from "../../styles/css/pages/Member/Modify.module.css";
 
 // Components
-import ChangePassword from "./ModifyMemberInfo/ChangePassword";
-import DeliveryRegister from "./ModifyMemberInfo/DeliveryRegister";
+import ChangePassword from "../../components/ModifyMemberInfo/ChangePassword";
+import DeliveryRegister from "../../components/ModifyMemberInfo/DeliveryRegister";
 
 const Modify = () => {
-  return <div>마이 페이지</div>;
+  return (
+    <div>
+      <div
+        style={{
+          width: "100%",
+          height: "202px",
+          backgroundColor: "teal",
+          position: "fixed",
+          zIndex: "20",
+          top: "0",
+        }}
+      >
+        헤더임
+      </div>
+      <div className={styles["modify-container"]}>
+        <div className={styles["modify-userInfo"]}>회원정보 카드</div>
+        <div className={styles["modify-userInfo__edit"]}>
+          <ChangePassword />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Modify;
