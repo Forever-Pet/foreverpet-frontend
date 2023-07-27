909319738606c4d5338c58210f64c1a112ddb4aa
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-07-27 23:08:48 +0900

    Feat: 상품 상세 페이지 url params 셋팅

--- src/pages/Product/ProductDetail.jsx
+++ src/pages/Product/ProductDetail.jsx
@@ -1,18 +1,25 @@
+// React
+import { useParams } from "react-router-dom";
+
 // CSS
 import styles from "../../styles/css/pages/Product/ProductDetail.module.css";
 
 // Components
 import ProductDetailInformation from "../../components/Product/ProductDetailInformation";
 import ProductDetailPriceCard from "../../components/Card/Detail/ProductDetailPriceCard";
+import { useEffect } from "react";
 
 const ProductDetail = () => {
+  const { id } = useParams();
+  useEffect(() => {
+    // 해당 params id로 상품 ID 요청
+    console.log(id);
+  });
   return (
     <div className={styles["product-detail__view-wrap"]}>
       <div className={styles["header"]}>헤더임</div>
 
-
       <div className={styles["product-detail__view-container"]}>
-
         <div className={styles["product-detail__view--information"]}>
           <ProductDetailInformation />
         </div>
@@ -20,7 +27,6 @@ const ProductDetail = () => {
         <div className={styles["product-detail__view--PriceCard"]}>
           <ProductDetailPriceCard />
         </div>
-        
       </div>
     </div>
   );
