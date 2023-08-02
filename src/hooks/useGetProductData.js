0e13e670635db9c6b1cb876eb039adc102ac8df8
Bella <88245113+shineju@users.noreply.github.com>
2023-08-02 18:23:31 +0900

    커스텀훅 재사용위한 수정
    커스텀훅 재사용위한 수정

--- src/hooks/useGetProductData.js
+++ src/hooks/useGetProductData.js
@@ -1,21 +1,26 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 
-const useGetProductData = (type, list, dataProps = false) => {
+const useGetProductData = (params, obj, search = false) => {
   const [data, setData] = useState(null)
 
-  const getData = (type, list) => {
-    if (dataProps) {
-
+  const getData = (params, obj) => {
+    if (search) {
+      //임시
     } else {
-      axios.get('http://ec2-15-164-206-172.ap-northeast-2.compute.amazonaws.com/' + type)
+      axios.get('http://ec2-15-164-206-172.ap-northeast-2.compute.amazonaws.com/' + params)
         .then((result) => result.data)
         .then((result) => {
-          if (typeof (list) == 'string') {
-            const found = result.filter(e => e.categories === list)
-            setData(found)
-          } else {
-            const found = result.filter(e => list.includes(e.id))
+          if (typeof (obj) == 'string') {
+            if (search) { //임시
+              setData(result)
+            } else {
+              const found = result.filter(e => e.categories === obj)
+              setData(found)
+            }
+          }
+          else {
+            const found = result.filter(e => obj.includes(e.id))
             setData(found)
           }
         })
@@ -26,7 +31,7 @@ const useGetProductData = (type, list, dataProps = false) => {
   }
 
   useEffect(() => {
-    getData(type, list)
+    getData(params, obj)
   }, [])
 
 
