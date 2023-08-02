6ea3078fe45df94eb4c19e845118dcc66f7d165c
최은하 <choieunha@choeeunhaui-MacBookAir.local>
2023-08-02 22:15:25 +0900

    변경 없음

--- src/pages/Product/ProductDetail.jsx
+++ src/pages/Product/ProductDetail.jsx
@@ -9,8 +9,6 @@ const ProductDetail = () => {
   return (
     <div className={styles["product-detail__view-wrap"]}>
       <div className={styles["header"]}>헤더임</div>
-
-
       <div className={styles["product-detail__view-container"]}>
 
         <div className={styles["product-detail__view--information"]}>
