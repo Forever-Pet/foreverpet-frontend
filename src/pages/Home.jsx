5139d2c1d1363ff6d71ac80e716c484a1c9a71d0
최은하 <choieunha@choeeunhaui-MacBookAir.local>
2023-08-04 14:29:23 +0900

    메인홈 간식 api (백엔드 요청 기달중))

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,9 @@ import MainSwiper from '../components/Banner/MainSwiper';
 import Category from '../components/Navigation/Category/Category';
 import BestItem from '../components/Home/BestItem';
 import CartegoryItem from '../components/Card/CartegoryItem';
-import BestData from '../components/Card/BestData';
 import MainBanTwo from '../components/Banner/MainBanTwo';
 import NewItem from '../components/Home/NewItem';
 import PrdSnack from '../components/Home/PrdSnack';
-import Snack from '../components/Navigation/Category/Snack';
 
 //css
 import styles from '../styles/css/pages/Home.module.css';
@@ -27,7 +25,7 @@ const Home = () => {
       <img src='img/eveban1.jpg' alt='' className={styles.home_ban_img}/>
       <img src='img/eveban2.jpg' alt='' className={styles.home_ban_img}/>
     </div>
-    <PrdSnack snackSlide={Snack}/>
+    <PrdSnack/>
     {/* <SubSlider carouselData={CarouselData}/> */}
     <Footer/>
   </div>
