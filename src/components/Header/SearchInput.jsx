a429eaab7df640c0219a80af96efa58471f76563
최은하 <choieunha@192.168.nate.com>
2023-08-05 12:22:40 +0900

    검색기능 개발 완료

--- src/components/Header/SearchInput.jsx
+++ src/components/Header/SearchInput.jsx
@@ -1,11 +1,13 @@
 import React, { useRef, useState } from 'react';
+
+//components
 import Input from '../../common/Input/Input';
-import Button from '../../common/Button/Button';
 import { debounce } from 'lodash';
 import usePathMove from '../../hooks/usePathMove';
 
 import styles from '../../styles/css/components/Header/searchInput.module.css';
 
+//icon
 import {GoSearch} from "react-icons/go";
 
 //헤더 검색창
@@ -13,32 +15,30 @@ const SearchInput = () => {
   const inputRef = useRef(null);
   const move = usePathMove();
 
-  const [search , setSearch] = useState({name: ""});
-  const {name} = Input;
+  const [search , setSearch] = useState("");
+  // const {name} = Input;
   const userSearch = (e) => {
-    const {name,value} = e.target;
-   
-    setSearch(e.target.value);
-    console.log(e.target.value);
+    console.log("inputRef.current.value::",inputRef.current.value)
+    setSearch(inputRef.current.value);
+    // console.log(e.target.value);
   };
 
   const getInputValueInfo = debounce(userSearch, 300);
   return (
-    <form className={styles.search_wrap}>
+    <div className={styles.search_wrap}>
       <Input
       type= "text"
       name = "productName"
       placeholder="우리 댕냥 알러지 없는 사료" 
       className="searchInput"
       ref={inputRef} 
-      value={name}
       onChange={getInputValueInfo}
       />
-       
+    
       <GoSearch className={styles.search_button} onClick={()=>{
         if(search.length==0) return;
         move("/product/search", {search}, true)}}></GoSearch> 
-    </form>
+    </div>
   );
 };
 
--- src/components/Header/SearchList.jsx
+++ src/components/Header/SearchList.jsx
@@ -5,16 +5,16 @@ import useGetProductData from '../../hooks/useGetProductData';
 
 const SearchList = () => {
   const location = useLocation();
-
   const title = location.state.search;
 
- const [data, getData] = useGetProductData(true, title);
-
-  return <>
+  const [data, getData] = useGetProductData(true, title);
+  
+  return (
     <div>
-      <Product data={data} getData={getData} title={title}/>
-    </div>
-    </>
+    <Product data={data} getData={getData} title={title}/>
+  </div>
+  )
+   
 };
 
 export default SearchList;
