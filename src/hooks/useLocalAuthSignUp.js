c1f8f37b134188d0c08035afc7959803a48dfd51
[LK1] <[ssomcandy777@gmail.com]>
2023-08-09 17:53:33 +0900

    Feat: 로그인 커스텀 훅 기능 추가

--- src/hooks/useLocalAuthSignUp.js
+++ src/hooks/useLocalAuthSignUp.js
@@ -2,11 +2,11 @@ import axios from "axios";
 import { useState } from "react";
 
 const useLocalAuthSignUp = () => {
-  const [responsive, setResponsive] = useState([]);
+  const [responsive, setResponsive] = useState(null);
 
   const authSingUp = async (url, bodyData) => {
     const API_URL = `http://ec2-15-164-206-172.ap-northeast-2.compute.amazonaws.com/${url}`;
-    const res = await axios.post(API_URL, { data: bodyData });
+    const res = await axios.post(API_URL, bodyData);
     setResponsive(res);
   };
   return [responsive, authSingUp];
