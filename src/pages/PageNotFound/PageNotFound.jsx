008711422d8d99e914755c09fe800034013e73d0
lk1 <whitecandy777@naver.com>
2023-08-14 21:00:37 +0900

    Design: 404 페이지 디자인 작업

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,15 +1,27 @@
+// CSS
+import styles from "./PageNotFound.module.css";
+
 // Hooks
 import usePathMove from "../../hooks/usePathMove";
 
 // Components
+import Title from "../../common/Title/Title";
 import Button from "../../common/Button/Button";
 
 const PageNotFound = () => {
   const pathMove = usePathMove();
   const rootPathMove = () => pathMove("/");
   return (
-    <div>
-      <Button title="홈으로" onClick={rootPathMove} />
+    <div className={styles["no_page"]}>
+      <div className={styles["no_page--container"]}>
+        <Title className="no_page--title" title="404" />
+        <div>해당 페이지는 개발 중이에요 ㅜㅜ</div>
+        <Button
+          title="홈으로"
+          onClick={rootPathMove}
+          className="no_page--btn"
+        />
+      </div>
     </div>
   );
 };
