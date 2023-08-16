d980bad53b437aa5a14f50653f39692038a420c7
unknown <whitecandy777@naver.com>
2023-08-16 18:07:15 +0900

    Feat: 카카오 로그인 API 연동 기능 추가

--- src/components/Auth/Kakao/KakaoCallback.jsx
+++ src/components/Auth/Kakao/KakaoCallback.jsx
@@ -1,38 +1,34 @@
+// React Hooks
 import { useEffect } from "react";
+
+// Custom Hooks
+import usePathMove from "../../../hooks/usePathMove";
+
 import axios from "axios";
 
 const KakaoCallback = () => {
+  const pathMove = usePathMove();
   useEffect(() => {
     const kakaoDataAxios = async () => {
       try {
         const params = new URL(document.location.toString()).searchParams;
         const CODE = params.get("code");
-        const GRANT_TYPE = "authorization_code";
-        const CLIENT_ID = "a6bfe93b460dd365f9951854df478ab9";
-        const REDIRECT_URL = "http://localhost:5173/oauth/kakaoCallback";
-        const KAKAO_ACCESS_URL = `https://kauth.kakao.com/oauth/token?grant_type=${GRANT_TYPE}&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&code=${CODE}`;
-        const KAKAO_USER_INFO_URL = `https://kapi.kakao.com/v2/user/me`;
-        const ACCESS_HEADERS_CONTENT_TYPE = {
-          "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-        };
-        const res = await axios.post(
-          KAKAO_ACCESS_URL,
-          {},
-          {
-            headers: ACCESS_HEADERS_CONTENT_TYPE,
-          }
-        );
-        const { access_token } = res.data;
         const body = {
-          authorizationCode: access_token,
+          authorizationCode: CODE,
         };
         console.log(body);
         const serverRes = await axios.post(
           "http://ec2-15-164-206-172.ap-northeast-2.compute.amazonaws.com/user/kakao",
           body
         );
-        console.log(body);
-        console.log(access_token);
+        console.log(serverRes);
+        if (serverRes.status === 200) {
+          sessionStorage.setItem("auth", serverRes.data.accessToken);
+          sessionStorage.setItem("kakao", serverRes.data.userSocialType);
+          alert("로그인 되었습니다.");
+          pathMove("/");
+          return;
+        }
       } catch (error) {
         console.log(error);
       }
