7cb21aa25eaaa8544ab549da2f8c88398f71091b
lk1 <whitecandy777@naver.com>
2024-01-11 20:43:01 +0900

    [Fix] - api key 환경변수 마무리

--- src/components/Auth/Kakao/KakaoAuthUserAccount.jsx
+++ src/components/Auth/Kakao/KakaoAuthUserAccount.jsx
@@ -3,8 +3,9 @@ import styles from "../../../styles/css/components/Auth/SNS/SNS.module.css";
 
 const KakaoAuthUserAccount = () => {
   const CLIENT_ID = "a6bfe93b460dd365f9951854df478ab9";
-  const REDIRECT_URL =
-    "http://ec2-13-209-75-154.ap-northeast-2.compute.amazonaws.com:3000/oauth/kakaoCallback";
+  const REDIRECT_URL = `${
+    import.meta.env.VITE_KAKAO_AUTH_API
+  }/oauth/kakaoCallback`;
   const URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&response_type=code`;
 
   return (
