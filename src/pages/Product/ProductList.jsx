897c13f7d63a2abde343488c66b942f3d6616902
lk1 <whitecandy777@naver.com>
2024-01-16 22:54:12 +0900

    [Fix] - 배포오류 수정

--- src/pages/Product/ProductList.jsx
+++ src/pages/Product/ProductList.jsx
@@ -1,24 +1,30 @@
-import { useEffect } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
-import Product from '../../components/Product/Product';
-import useGetProductData from '../../hooks/useGetProductData'
+import { useLocation, useParams } from "react-router-dom";
+import Product from "../../components/Product/Product";
+import useGetProductData from "../../hooks/useGetProductData";
 
 const ProductList = () => {
   //전달받은 타이틀(한글명), 카테고리 전달
   const location = useLocation();
-  const title = location.state.title
+  const title = location.state.title;
 
   //url파라미터로 해당 카테고리 데이터 가져옴
-  const param = useParams()
-  const category = param.category
+  const param = useParams();
+  const category = param.category;
 
-  const [data, getData] = useGetProductData()
+  const [data, getData] = useGetProductData();
 
-  return <>
-    <div>
-      <Product data={data} getData={getData} title={title} category={category} />
-    </div>
-  </>
+  return (
+    <>
+      <div>
+        <Product
+          data={data}
+          getData={getData}
+          title={title}
+          category={category}
+        />
+      </div>
+    </>
+  );
 };
 
 export default ProductList;
